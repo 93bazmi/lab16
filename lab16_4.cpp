5a3da0ea290986cf1fef4330d83a4cd65e1f0d07
bazmi <bambamboom234@gmail.com>
2023-02-14 04:50:49 +0700

    lab16_4.cpp completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,169 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &s,int &n,int &o,int &w){
+	int a = s , b = n , c = o , d = w ; 
+	int x = rand()%4 ; 
+	if(x==0){
+		s = a;
+		int y=rand()%3;
+		if(y==0){
+			n=b;
+			int z=rand()%2;
+			if(z==0){
+				o=c;
+				w=d;
+			}
+			if(z==1){
+				w=c;
+				o=d;
+			}
+		}
+		if(y==1){
+			o=b;
+			int z=rand()%2;
+			if(z==0){
+				n=c;
+				w=d;
+			}
+			if(z==1){
+				w=c;
+				n=d;
+			}
+		}
+		if(y==2){
+			w=b;
+			int z=rand()%2;
+			if(z==0){
+				n=c;
+				o=d;
+			}
+			if(z==1){
+				o=c;
+				n=d;
+			}
+		}
+	}
+	if(x==1){
+		n = a;
+		int y=rand()%3;
+		if(y==0){
+			s=b;
+			int z=rand()%2;
+			if(z==0){
+				o=c;
+				w=d;
+			}
+			if(z==1){
+				w=c;
+				o=d;
+			}
+		}
+		if(y==1){
+			o=b;
+			int z=rand()%2;
+			if(z==0){
+				s=c;
+				w=d;
+			}
+			if(z==1){
+				w=c;
+				s=d;
+			}
+		}
+		if(y==2){
+			w=b;
+			int z=rand()%2;
+			if(z==0){
+				s=c;
+				o=d;
+			}
+			if(z==1){
+				o=c;
+				s=d;
+			}
+		}
+	}
+	if(x==2){
+		o = a;
+		int y=rand()%3;
+		if(y==0){
+			s=b;
+			int z=rand()%2;
+			if(z==0){
+				n=c;
+				w=d;
+			}
+			if(z==1){
+				w=c;
+				n=d;
+			}
+		}
+		if(y==1){
+			n=b;
+			int z=rand()%2;
+			if(z==0){
+				s=c;
+				w=d;
+			}
+			if(z==1){
+				w=c;
+				s=d;
+			}
+		}
+		if(y==2){
+			w=b;
+			int z=rand()%2;
+			if(z==0){
+				s=c;
+				n=d;
+			}
+			if(z==1){
+				n=c;
+				s=d;
+			}
+		}
+	}
+	if(x==3){
+		w = a;
+		int y=rand()%3;
+		if(y==0){
+			s=b;
+			int z=rand()%2;
+			if(z==0){
+				n=c;
+				o=d;
+			}
+			if(z==1){
+				o=c;
+				n=d;
+			}
+		}
+		if(y==1){
+			n=b;
+			int z=rand()%2;
+			if(z==0){
+				s=c;
+				o=d;
+			}
+			if(z==1){
+				o=c;
+				s=d;
+			}
+		}
+		if(y==2){
+			o=b;
+			int z=rand()%2;
+			if(z==0){
+				s=c;
+				n=d;
+			}
+			if(z==1){
+				n=c;
+				s=d;
+			}
+		}
+	}
+}
+
